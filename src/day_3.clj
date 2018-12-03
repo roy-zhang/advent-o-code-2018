02be2663bcb9540c72f7a40d5e66ea97805ac512
Roy Zhang <i.royzhang@gmail.com>
2018-12-03 14:23:07 -0800

    day 3

--- src/day_3.clj
+++ src/day_3.clj
@@ -3,18 +3,47 @@
 
 (utils/get-str-input "input/3")
 
-;; dumb way is a [][] containing a list of ids
-;; another way is combo, and a function that compares two patches
-;; loop big-map remaining-list overlaps
-
 ;; id inches-left inches-top width height
-(defn patch-str->patch [patch-str]
+(defn str->patch [patch-str]
   (map read-string (re-seq #"[0-9]+" patch-str)))
 
 (defn points [[id x y width height]]
   (let [xx (+ x width -1)
         yy (+ y height -1)]
-    [[x y] [xx y] [xx yy] [x yy]]))
+    [id [x y] [xx yy]]))
 
+(defn all-points [[id [tlx tly][brx bry]]]
+  (utils/cartesian-product (range tlx (inc brx)) (range tly (inc bry))))
 
-(defn overlaps? [patch-a patch-b])
+(defn points-within [[id [tlx tly] [brx bry]] points]
+  (let [in? (fn [[px py]]
+              (and (>= px tlx) (<= px brx)
+                   (>= py tly) (<= py bry)))]
+    (filter in? (all-points points))))
+
+(defn part-1 []
+  (let [input (->> (utils/get-str-input "input/3")
+                   (map (comp points str->patch)))
+        combos (->> (utils/cartesian-product input input)
+                    (remove #(>= (ffirst %) (first (second %)))))]
+    (count
+      (reduce (fn [overlapers [points-1 points-2]]
+                (apply (partial conj overlapers) (points-within points-1 points-2)))
+              #{} combos))))
+
+(defn overlaps? [points-a points-b]
+  (not (empty? (points-within points-a points-b))))
+
+(defn find-unique-patch [input]
+  (let [combos (->> (utils/cartesian-product input input)
+                    (remove #(>= (ffirst %) (first (second %)))))]
+    (reduce (fn [unoverlapped [points-1 points-2]]
+              (if (overlaps? points-1 points-2)
+                (disj unoverlapped points-1 points-2)
+                unoverlapped))
+            (set input) combos)))
+
+(defn part-2 []
+  (->> (utils/get-str-input "input/3")
+       (map (comp points str->patch))
+       find-unique-patch))
